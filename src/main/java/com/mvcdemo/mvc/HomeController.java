3904f746cfafbb573902d1ac54006590530f2e7a
mszpila <szpila.mikolaj@gmail.com>
2021-03-11 18:41:48 +0100

    init

---
+++ src/main/java/com/mvcdemo/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.mvcdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "main-page";
+    }
+}
